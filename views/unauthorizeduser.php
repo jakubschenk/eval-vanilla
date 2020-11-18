7c0af3c6906b24b15bd4846c4a4b1943c10bf2e5
Abuk <abukcz@gmail.com>
2020-11-18 14:26:55 +0100

    unath: fix derp

--- views/unauthorizeduser.php
+++ views/unauthorizeduser.php
@@ -2,9 +2,9 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <title>evaluační nástroj spšei</title>
     <style>
         * {
@@ -39,15 +39,10 @@
         
     </style>
 </head>
-
 <body>
-
     <div class="main-div">
-        <img src="public/images/spsei-logo.png">
+        <img src="public/images/spsei-logo.png"/>
         <p>Nejste v databázi povolených uživatelů pro tento školní rok!</p>
-        </div>
     </div>
-
 </body>
-
 </html>
