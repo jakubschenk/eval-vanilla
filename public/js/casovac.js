da391b5a8e1826f115c31ac2edfa371f3902cb60
Abuk <abukcz@gmail.com>
2021-03-19 14:55:20 +0100

    eval: fix refresh loop bug

--- public/js/casovac.js
+++ public/js/casovac.js
@@ -1,25 +1,28 @@
 $(document).ready(function () {
     if (cas_od !== "") {
         var countTo = new Date(cas_od).getTime();
-
-        var x = setInterval(function () {
-            var ted = new Date().getTime();
-            var vzdalenost = countTo - ted;
-
-            var dny = Math.floor(vzdalenost / (1000 * 60 * 60 * 24));
-            var hodiny = Math.floor((vzdalenost % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minuty = Math.floor((vzdalenost % (1000 * 60 * 60)) / (1000 * 60));
-            var sekundy = Math.floor((vzdalenost % (1000 * 60)) / 1000);
-
-            $("#clock").text(dny + "d " + hodiny + "h " + minuty + "m " + sekundy + "s ");
-
-            if (vzdalenost < 0) {
-                clearInterval(x);
-                location.reload();
-            }
-        }, 1000);
+        var ted = new Date().getTime();
+        if(countTo > ted) {
+            var x = setInterval(function () {
+                var vzdalenost = countTo - ted;
+                ted = new Date().getTime();
+                var dny = Math.floor(vzdalenost / (1000 * 60 * 60 * 24));
+                var hodiny = Math.floor((vzdalenost % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minuty = Math.floor((vzdalenost % (1000 * 60 * 60)) / (1000 * 60));
+                var sekundy = Math.floor((vzdalenost % (1000 * 60)) / 1000);
+    
+                $("#clock").text(dny + "d " + hodiny + "h " + minuty + "m " + sekundy + "s ");
+    
+                if (vzdalenost < 0) {
+                    clearInterval(x);
+                    location.reload();
+                }
+            }, 1000);    
+        } else {
+            $("#clock").text("Evaluace již proběhla");
+        }     
     } else {
-        $("#clock").text("Není nastaven!");
+        $("#clock").text("Čas není nastaven");  
     }
 
 });
